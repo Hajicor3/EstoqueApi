fdef268052931aa474942239e343379ff0d18f37
Hajicor3 <gabriel.mirandawv@outlook.com>
2025-03-18 16:36:59 -0300

    Adicionando o rabbitMQ e a classe listener do metodo de cancelar mocimentação e exclusão do metodo de cancelar movimentação do controller

--- src/main/java/com/hajicor3/EstoqueApi/controllers/MovimentacaoController.java
+++ src/main/java/com/hajicor3/EstoqueApi/controllers/MovimentacaoController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,17 +43,7 @@ public class MovimentacaoController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(movimentacao.getId()).toUri();
 		return ResponseEntity.created(uri).body(movimentacao);
 	}
-	
-	@Operation(description = "Cancela uma movimentacao já feita, pelo id.")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "cancela uma movimentacao."),
-			@ApiResponse(responseCode = "404", description = "Não existe movimentacao no id informado.")
-	})
-	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> cancelarMovimentacaoPorid(@PathVariable Long id){
-		movimentacaoService.cancelarMovimentacao(id);
-		return ResponseEntity.noContent().build();
-	}
+
 	@Operation(description = "Retorna uma lista de Dto´s de todas as movimentações do banco.")
 	@ApiResponses(value = @ApiResponse(responseCode = "200",description = "Retorna uma lista de todas as movimentações."))
 	@GetMapping
---
+++ src/main/java/com/hajicor3/EstoqueApi/repositories/mqueues/EstoqueProdutoSubscriber.java
@@ -0,0 +1,39 @@
+package com.hajicor3.EstoqueApi.repositories.mqueues;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hajicor3.EstoqueApi.services.MovimentacaoService;
+import com.hajicor3.EstoqueApi.services.exceptions.ResourceNotFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class EstoqueProdutoSubscriber {
+	
+	@Autowired
+	private MovimentacaoService movimentacaoService;
+	
+	@RabbitListener(queues = "${mq.queue.produto-estoque-queue}")
+	public void receberSolicitacaoCancelamentoMovimentacao(@Payload String payload) {
+		try {
+			
+			ObjectMapper mapper = new ObjectMapper();
+			Long id = mapper.readValue(payload, Long.class);
+			movimentacaoService.cancelarMovimentacao(id);
+		}
+		catch(NumberFormatException e) {
+			log.error("Erro ao converter o id da mensagem: " + Long.parseLong(payload) + e);
+		}
+		catch(ResourceNotFoundException e) {
+			log.warn("Movimentação não encontrada para o ID: " + Long.parseLong(payload));
+		}
+		catch(Exception e){
+			log.error("Erro inesperado ao processar a solicitação de cancelamento de movimentação", e);
+		}
+	}
+}
